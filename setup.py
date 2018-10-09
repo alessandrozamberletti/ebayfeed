74f3996663a035f889f5670e19b9e2d0b5f44ad3
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-10-09 11:11:49 +0200

    * auto pypi deploy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='ebayfeed',
-    version='2018.10.04',
+    version='2018.10.05',
     author='alez',
     author_email='alez.pypi@gmail.com',
     url='https://github.com/alessandrozamberletti/ebayfeed',
