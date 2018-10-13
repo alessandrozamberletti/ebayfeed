e0972fc26a3287b6900daef8dba7274a2fe83b8c
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-10-13 10:49:14 +0200

    * ops

--- ebayfeed/__init__.py
+++ ebayfeed/__init__.py
@@ -3,3 +3,4 @@ from ebayfeed.constants import *
 from ebayfeed.api import Api
 from ebayfeed.credentials import Credentials
 from ebayfeed.downloader import get_feed
+from ebayfeed.categories import get_macro_categories
