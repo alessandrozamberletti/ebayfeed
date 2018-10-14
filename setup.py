7c81d5229a5bc9b7396ab56a98721d41f9d98b5d
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-10-14 16:53:13 +0200

    * new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='ebayfeed',
-    version='2018.10.7',
+    version='2018.10.8',
     author='alez',
     author_email='alez.pypi@gmail.com',
     url='https://github.com/alessandrozamberletti/ebayfeed',
