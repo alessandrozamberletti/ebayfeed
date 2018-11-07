ecf64dafbd62a9d17b77292791c5d2c5ca167479
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-07 17:29:06 +0100

    * refactoring

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase, main
-from tests import credentials
+from tests import credentials, a_category, a_marketplace
 from datetime import datetime
 
 from ebayfeed.downloader import get_feed
 from ebayfeed.categories import get_macro_categories
-from ebayfeed.constants import SCOPE_ALL_ACTIVE, SCOPE_NEWLY_LISTED, EBAY_US
+from ebayfeed.constants import SCOPE_ALL_ACTIVE, SCOPE_NEWLY_LISTED
 
 
 class TestAcceptance(TestCase):
     def test_get_feed_all_active(self):
-        get_feed(credentials, 3252, SCOPE_ALL_ACTIVE, EBAY_US)
+        get_feed(credentials, a_category, SCOPE_ALL_ACTIVE, a_marketplace)
 
     def test_get_feed_newly_listed(self):
         yesterday = datetime.today().strftime("%Y%m%d")
-        get_feed(credentials, 3252, SCOPE_NEWLY_LISTED, EBAY_US, date=yesterday)
+        get_feed(credentials, a_category, SCOPE_NEWLY_LISTED, a_marketplace, date=yesterday)
 
     def test_get_macro(self):
-        get_macro_categories(credentials, EBAY_US)
+        get_macro_categories(credentials, a_marketplace)
 
 
 if __name__ == "__main__":
